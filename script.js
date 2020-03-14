f00adff8446bc17fd0824166e7e86c54f93e558c
mscoulte <mscoulte@gmail.com>
2020-03-14 16:34:55 -0400

    Finished work on JS. Local storage now loading and button click now adds/replaces current LS key value.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-currentHour = moment().format("H");
+let currentHour = moment().format("H");
 
 $(document).ready(function() {
   currentDay = moment().format("dddd, MMMM Do YYYY");
@@ -6,7 +6,51 @@ $(document).ready(function() {
 
   console.log(currentHour);
   checkTime();
+  getStorage();
+  addStorage();
 });
+
+function getStorage() {
+  $("#9txt").html(localStorage.getItem("9AM"));
+  $("#10txt").html(localStorage.getItem("10AM"));
+  $("#11txt").html(localStorage.getItem("11AM"));
+  $("#12txt").html(localStorage.getItem("12PM"));
+  $("#1txt").html(localStorage.getItem("1PM"));
+  $("#2txt").html(localStorage.getItem("2PM"));
+  $("#3txt").html(localStorage.getItem("3PM"));
+  $("#4txt").html(localStorage.getItem("4PM"));
+  $("#5txt").html(localStorage.getItem("5PM"));
+}
+
+function addStorage() {
+  $("#9btn").on("click", function() {
+    localStorage.setItem("9AM", $("#9txt").val());
+  });
+  $("#10btn").on("click", function() {
+    localStorage.setItem("10AM", $("#10txt").val());
+  });
+  $("11btn").on("click", function() {
+    localStorage.setItem("11AM", $("#11txt").val());
+  });
+  $("#12btn").on("click", function() {
+    localStorage.setItem("12PM", $("#12txt").val());
+  });
+  $("#1btn").on("click", function() {
+    localStorage.setItem("1PM", $("#1txt").val());
+  });
+  $("#2btn").on("click", function() {
+    localStorage.setItem("2PM", $("#2txt").val());
+  });
+  $("#3btn").on("click", function() {
+    localStorage.setItem("3PM", $("#3txt").val());
+  });
+  $("#4btn").on("click", function() {
+    localStorage.setItem("4PM", $("#4txt").val());
+  });
+  $("#5btn").on("click", function() {
+    localStorage.setItem("5PM", $("#5txt").val());
+  });
+}
 
 function checkTime() {
   if (currentHour < 9) {

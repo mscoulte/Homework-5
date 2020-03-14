12f9c81491a914f6b3cfef43f41b2ad65b9eac32
mscoulte <mscoulte@gmail.com>
2020-03-14 15:53:45 -0400

    Completed work on JS to get past, present and future classes added to the time blocks based on the current hour

--- script.js
+++ script.js
@@ -1,4 +1,75 @@
+currentHour = moment().format("H");
+
 $(document).ready(function() {
-  currentDay = moment().format("MMMM Do YYYY");
+  currentDay = moment().format("dddd, MMMM Do YYYY");
   let current = $("#currentDay").text(currentDay);
+
+  console.log(currentHour);
+  checkTime();
 });
+
+function checkTime() {
+  if (currentHour < 9) {
+    $("#9txt").addClass("future");
+  } else if (currentHour == 9) {
+    $("#9txt").addClass("present");
+  } else {
+    $("#9txt").addClass("past");
+  }
+  if (currentHour < 10) {
+    $("#10txt").addClass("future");
+  } else if (currentHour == 10) {
+    $("#10txt").addClass("present");
+  } else {
+    $("#10txt").addClass("past");
+  }
+  if (currentHour < 11) {
+    $("#11txt").addClass("future");
+  } else if (currentHour == 11) {
+    $("#11txt").addClass("present");
+  } else {
+    $("#11txt").addClass("past");
+  }
+  if (currentHour < 12) {
+    $("#12txt").addClass("future");
+  } else if (currentHour == 12) {
+    $("#12txt").addClass("present");
+  } else {
+    $("#12txt").addClass("past");
+  }
+  if (currentHour < 13) {
+    $("#1txt").addClass("future");
+  } else if (currentHour == 13) {
+    $("#1txt").addClass("present");
+  } else {
+    $("#1txt").addClass("past");
+  }
+  if (currentHour < 14) {
+    $("#2txt").addClass("future");
+  } else if (currentHour == 14) {
+    $("#2txt").addClass("present");
+  } else {
+    $("#2txt").addClass("past");
+  }
+  if (currentHour < 15) {
+    $("#3txt").addClass("future");
+  } else if (currentHour == 15) {
+    $("#3txt").addClass("present");
+  } else {
+    $("#3txt").addClass("past");
+  }
+  if (currentHour < 16) {
+    $("#4txt").addClass("future");
+  } else if (currentHour == 16) {
+    $("#4txt").addClass("present");
+  } else {
+    $("#4txt").addClass("past");
+  }
+  if (currentHour < 17) {
+    $("#5txt").addClass("future");
+  } else if (currentHour == 17) {
+    $("#5txt").addClass("present");
+  } else {
+    $("#5txt").addClass("past");
+  }
+}
